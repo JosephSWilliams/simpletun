2e67df22919be7ea16798a34ff5a26d4336f72a6
root <root@d3v11.ano>
2013-02-19 16:56:43 +0000

    [simpletun][.gitignore, curvecpclientauth.c]   ignore nacl-*/ for git   checks with better exit status

--- src/curvecpclientauth.c
+++ src/curvecpclientauth.c
@@ -1,7 +1,10 @@
 #define USAGE "Usage: curvecpclientauth CURVECPCLIENTPUBKEY prog [args]\n"
 int main(int argc, char **argv){
 if ((argc>2)&&(strlen(argv[1])>=64)){
-  if (memcmp(getenv("CURVECPCLIENTPUBKEY"),argv[1],64)) exit(0); /* expects lowercase */
-  if (execvp(argv[2],argv+2)<0) exit(255);}
+  if (!getenv("CURVECPCLIENTPUBKEY")) exit(64);
+  if (strlen(getenv("CURVECPCLIENTPUBKEY"))<64) exit(64);
+  if (memcmp(getenv("CURVECPCLIENTPUBKEY"),argv[1],64)) exit(1); /* expects lowercase */
+  execvp(argv[2],argv+2);
+  exit(255);}
 write(2,USAGE,strlen(USAGE));
 exit(64);}
