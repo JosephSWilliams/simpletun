67c093edd0931c54ffd3be9cb95a41535bbdd0f5
root <root@d3v11.ano>
2013-02-20 17:50:23 +0000

    test

--- src/tunserver.c
+++ src/tunserver.c
@@ -1,36 +1,33 @@
 #include <poll.h>
 main(){
 
-struct pollfd fds[4];
+struct pollfd fds[2];
 fds[0].fd = 0;
 fds[0].events = 3;
 fds[0].revents = 3;
 fds[1].fd = 3;
 fds[1].events = 3;
 fds[1].revents = 3;
-fds[2].fd = 3;
-fds[2].events = 4;
-fds[2].revents = 4;
 
 unsigned char p[1502] = {0};
 int i=0, n=0, len=0;
 
 while (1){
 
-  if ((poll(&fds[0],1,256)>0)&&(poll(&fds[2],1,256)>0)){
+  if (poll(&fds[0],1,256)>0){
     len=read(0,p,2);
-    if (2>len) exit(0);
+    if (2>len) exit(1);
     len=p[0]*256+p[1];
-    if (1500<len) exit(0);
+    if (1500<len) exit(2);
     i=0; n=0;
     while(n<len){
       i=read(0,&p[n],len-n);
-      if (i<1) exit(0);
+      if (i<1) exit(3);
       n+=i;}
     write(3,p,len);}
 
   if (poll(&fds[1],1,256)>0){
     len=read(3,&p[2],1500);
-    if (0>len) exit(0);
+    if (0>len) exit(4);
     p[0]=len/256; p[1]=len%256;
     write(1,p,2+len);}}}
