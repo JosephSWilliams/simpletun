2102299542e2aec92dd1664edd180a4deae18bd9
root <root@d3v11.ano>
2013-02-20 17:46:20 +0000

    test

--- src/tunclient.c
+++ src/tunclient.c
@@ -8,13 +8,16 @@ fds[0].revents = 3;
 fds[1].fd = 3;
 fds[1].events = 3;
 fds[1].revents = 3;
+fds[2].fd = 3;
+fds[2].events = 4;
+fds[2].revents = 4;
 
 unsigned char p[1502] = {0};
 int i=0, n=0, len=0;
 
 while (1){
 
-  if (poll(&fds[0],1,256)>0){
+  if ((poll(&fds[0],1,256)>0)&&(poll(&fds[2],1,256)>0)){
     len=read(6,p,2);
     if (2>len) exit(0);
     len=p[0]*256+p[1];
