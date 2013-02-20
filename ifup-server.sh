f6ec2145191fcb28f24cb7a5525314e5059fa8bb
root <root@d3v11.ano>
2013-02-20 19:55:08 +0000

    [simpletun] fixes MTU

--- ifup-server.sh
+++ ifup-server.sh
@@ -8,6 +8,7 @@ export PTP=`cat env/PTP`
   ip addr add $TUN_ADDR peer $PTP/32 dev $INTERFACE scope link
   ip addr add $GATEWAY/32 dev $INTERFACE scope global
   ip link set dev $INTERFACE up
+  ifconfig $INTERFACE mtu 1024
   #ping -c 1 -I $INTERFACE $PTP -r -w 5
 ) &
 exec ./tun $INTERFACE ./tunserver
