446dc6e6cc5cfa04e0f8bb4e16fce8f87896d22d
root <root@d3v11.ano>
2013-02-23 03:19:21 +0000

    [tunserver.c, tunclient.c] keepalive (curvecpserver timeout workaround || test)

--- src/tunclient.c
+++ src/tunclient.c
@@ -1,5 +1,9 @@
+#include <sys/time.h>
 #include <poll.h>
 main(){
+
+struct timeval now, then;
+gettimeofday(&then,(struct timezone *)0);
 
 struct pollfd fds[2];
 fds[0].fd = 6;
@@ -14,10 +18,16 @@ int i=0, n=0, len=0;
 
 while (1){
 
+  gettimeofday(&now,(struct timezone *)0);
+  if (now.tv_sec-then.tv_sec>32){
+    gettimeofday(&then,(struct timezone *)0);
+    write(7,"\x00\x00",2);}
+
   if (poll(&fds[0],1,256)>0){
     len=read(6,p,2);
     if (2>len) exit(1);
     len=p[0]*256+p[1];
+    if (!len) continue;
     if (1500<len) exit(2);
     i=0; n=0;
     while(n<len){
