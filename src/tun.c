3837f031d314804fa4710c7079a65ba0254e572a
root <root@d3v11.ano>
2013-02-28 02:06:22 +0000

    [tun.c] bugfix (bsd)

--- src/tun.c
+++ src/tun.c
@@ -20,9 +20,9 @@ if ((argc>2)&&(strlen(argv[1])>0)&&(strlen(argv[1])<17)){
     memmove(&ifr_name[5],argv[1],strlen(argv[1]));
     if (open(ifr_name,2)!=3) exit(255);
     int ifr_flag = IFF_POINTOPOINT | IFF_MULTICAST;
-    if (ioctl(fd,TUNSIFMODE,&ifr_flag)<0) exit(255);
-    ifr_flag=0; if (ioctl(fd,TUNSLMODE,&ifr_flag)<0) exit(255);
-    ifr_flag=0; if (ioctl(fd,TUNSIFHEAD,&ifr_flag)<0) exit(255);
+    if (ioctl(3,TUNSIFMODE,&ifr_flag)<0) exit(255);
+    ifr_flag=0; if (ioctl(3,TUNSLMODE,&ifr_flag)<0) exit(255);
+    ifr_flag=0; if (ioctl(3,TUNSIFHEAD,&ifr_flag)<0) exit(255);
   #endif
   execvp(argv[2],argv+2);}
 write(2,USAGE,strlen(USAGE));
