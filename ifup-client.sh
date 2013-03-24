afb86bda98f828b940185791be0d70a13f28041c
root <root@d3v11.ano>
2013-03-24 17:42:33 +0000

    [ifup-*.sh] env/MTU [UPDATE NOTICE] admins MUST `printf 1024 > env/MTU'

--- ifup-client.sh
+++ ifup-client.sh
@@ -3,12 +3,12 @@ export INTERFACE=`cat env/INTERFACE`
 export TUN_ADDR=`cat env/TUN_ADDR`
 export GATEWAY=`cat env/GATEWAY`
 export PTP=`cat env/PTP`
+export MTU=`cat env/MTU`
 (
   sleep 4
   ip addr add $TUN_ADDR peer $PTP/32 dev $INTERFACE scope link
   ip addr add $GATEWAY/32 dev $INTERFACE scope global
   ip link set dev $INTERFACE up
-  ifconfig $INTERFACE mtu 1024
-  #ping -c 1 -I $INTERFACE $PTP -r -w 5
+  ifconfig $INTERFACE mtu $MTU
 ) &
 exec ./tun $INTERFACE ./tunclient
--- ifup-server.sh
+++ ifup-server.sh
@@ -3,13 +3,13 @@ export INTERFACE=`cat env/INTERFACE`
 export TUN_ADDR=`cat env/TUN_ADDR`
 export GATEWAY=`cat env/GATEWAY`
 export PTP=`cat env/PTP`
+export MTU=`cat env/MTU`
 ip link del $INTERFACE
 (
   sleep 4
   ip addr add $TUN_ADDR peer $PTP/32 dev $INTERFACE scope link
   ip addr add $GATEWAY/32 dev $INTERFACE scope global
   ip link set dev $INTERFACE up
-  ifconfig $INTERFACE mtu 1024
+  ifconfig $INTERFACE mtu $MTU
 ) &
-ping -c 1 -I $INTERFACE $PTP -r -w 5 &
 exec ./tun $INTERFACE ./tunserver
