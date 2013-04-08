fcc1fbcfbc014d8ac966aa743300dde107aa547e
root <root@d3v11.ano>
2013-04-08 12:47:33 +0000

    [simpletun/ifup-*.sh] bsd support, unstable (thanks epoch)

--- ifup-client.sh
+++ ifup-client.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 export INTERFACE=`cat env/INTERFACE`
 export TUN_ADDR=`cat env/TUN_ADDR`
 export GATEWAY=`cat env/GATEWAY`
@@ -6,9 +6,14 @@ export PTP=`cat env/PTP`
 export MTU=`cat env/MTU`
 (
   sleep 4
-  ip addr add $TUN_ADDR peer $PTP/32 dev $INTERFACE scope link
-  ip addr add $GATEWAY/32 dev $INTERFACE scope global
-  ip link set dev $INTERFACE up
-  ifconfig $INTERFACE mtu $MTU
+  if which ip; then
+    ip addr add $TUN_ADDR peer $PTP/32 dev $INTERFACE scope link
+    ip addr add $GATEWAY/32 dev $INTERFACE scope global
+    ip link set dev $INTERFACE up
+    ifconfig $INTERFACE mtu $MTU
+  else
+    ifconfig $INTERFACE create $TUN_ADDR $PTP netmask 255.255.255.255 mtu $MTU up
+    route add -host $PTP -link $INTERFACE -iface &
+  fi
 ) &
 exec ./tun $INTERFACE ./tunclient
--- ifup-server.sh
+++ ifup-server.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 export INTERFACE=`cat env/INTERFACE`
 export TUN_ADDR=`cat env/TUN_ADDR`
 export GATEWAY=`cat env/GATEWAY`
@@ -7,9 +7,14 @@ export MTU=`cat env/MTU`
 ip link del $INTERFACE
 (
   sleep 4
-  ip addr add $TUN_ADDR peer $PTP/32 dev $INTERFACE scope link
-  ip addr add $GATEWAY/32 dev $INTERFACE scope global
-  ip link set dev $INTERFACE up
-  ifconfig $INTERFACE mtu $MTU
+  if which ip; then
+    ip addr add $TUN_ADDR peer $PTP/32 dev $INTERFACE scope link
+    ip addr add $GATEWAY/32 dev $INTERFACE scope global
+    ip link set dev $INTERFACE up
+    ifconfig $INTERFACE mtu $MTU
+  else
+    ifconfig $INTERFACE create $TUN_ADDR $PTP netmask 255.255.255.255 mtu $MTU up
+    route add -host $PTP -link $INTERFACE -iface &
+  fi
 ) &
 exec ./tun $INTERFACE ./tunserver
