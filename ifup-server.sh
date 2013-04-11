718c730a28b042289d396c51fbd661b48b51d381
root <root@d3v11.ano>
2013-04-11 00:07:21 +0000

    [ifup-server.sh] bugfix

--- ifup-server.sh
+++ ifup-server.sh
@@ -4,10 +4,10 @@ export TUN_ADDR=`cat env/TUN_ADDR`
 export GATEWAY=`cat env/GATEWAY`
 export PTP=`cat env/PTP`
 export MTU=`cat env/MTU`
-ip link del $INTERFACE
 (
   sleep 4
   if which ip; then
+    ip link del $INTERFACE
     ip addr add $TUN_ADDR peer $PTP/32 dev $INTERFACE scope link
     ip addr add $GATEWAY/32 dev $INTERFACE scope global
     ip link set dev $INTERFACE up
