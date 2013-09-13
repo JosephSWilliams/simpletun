2eb82a49c70dd278f4b7f874a69f42d1a7cb13e2
root <root@d3v11.ano>
2013-09-13 03:42:47 +0000

    [src/recvpasswd.c][security] resist timing attacks

--- src/recvpasswd.c
+++ src/recvpasswd.c
@@ -1,10 +1,16 @@
 #define USAGE "Usage: recvpasswd passwd prog [args]\n"
-int main (int argc, char **argv) {
-if (argc>2){
-  char *passwd = (char *)malloc(sizeof(char)*strlen(argv[1]));
-  if (!passwd) exit(64);
-  read(0,passwd,strlen(argv[1]));
-  if (memcmp(passwd,argv[1],strlen(argv[1]))) exit(1);
-  execvp(argv[2],argv+2);}
-write(2,USAGE,strlen(USAGE));
-exit(64);}
+#include <stdlib.h>
+#include <string.h>
+int main (int argc, char *argv[]) {
+ int i, n, ret=0;
+ unsigned char passwd0[256]={0};
+ unsigned char passwd1[256]={0};
+ if ((argc>2)&&((n=strlen(argv[1])&255))) {
+  memcpy(passwd0,argv[1],n);
+  if (read(0,passwd1,256)<1) exit(32);
+  for (i=0;i<256;++i) ret |= (passwd0[i]==passwd1[i]) ? 1 : 0;
+  if (!n) execvp(argv[2],argv+2);
+ }
+ write(2,USAGE,strlen(USAGE));
+ exit(64);
+}
