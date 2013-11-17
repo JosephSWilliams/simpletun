9f641bbf76e5282cd708c21f63b4c2d121fa60e2
root <root@ip-10-56-75-16.(none)>
2013-11-17 17:00:24 +0000

    [recvpasswd.c] bugfix, comparison operator (thanks sfor)

--- src/recvpasswd.c
+++ src/recvpasswd.c
@@ -1,14 +1,14 @@
 #define USAGE "Usage: recvpasswd passwd prog [args]\n"
 #include <stdlib.h>
 #include <string.h>
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
  int i, n, ret=0;
  unsigned char passwd0[256]={0};
  unsigned char passwd1[256]={0};
  if ((argc>2)&&((n=strlen(argv[1])&255))) {
   memcpy(passwd0,argv[1],n);
   if (read(0,passwd1,n)<n) exit(32);
-  for (i=0;i<256;++i) ret |= (passwd0[i]==passwd1[i]) ? 1 : 0;
+  for (i=0;i<256;++i) ret |= (passwd0[i]!=passwd1[i]) ? 1 : 0;
   if (!ret) execvp(argv[2],argv+2);
  }
  write(2,USAGE,strlen(USAGE));
